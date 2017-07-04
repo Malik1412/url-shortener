d3933d89c3c72d127498ac8e2072ee8e894e3804
pruz_arias <abdulmalik1412@yahoo.com.my>
2017-07-04 15:19:25 +0800

    2nd up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'urls#index'
+  resources :urls
+
+  get "/short" => "urls#short"
 end
+
+#on line 6 like sinastra but /short would be your custom url/routes, in urls#short, urls#is your controller name and #short is the action available in the controller
